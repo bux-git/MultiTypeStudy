51cca21e5cec59024f52163349378cd0a2286047
bux <471025316@qq.com>
2017-05-19 11:53:36 +0800

    1.新增微博显示图片子内容

--- app/src/main/java/com/dqr/www/multitypestudy/weibo/WeiboActivity.java
+++ app/src/main/java/com/dqr/www/multitypestudy/weibo/WeiboActivity.java
@@ -1,18 +1,22 @@
 package com.dqr.www.multitypestudy.weibo;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import com.dqr.www.multitypestudy.MenuBaseActivity;
 import com.dqr.www.multitypestudy.R;
+import com.dqr.www.multitypestudy.weibo.content.SimpleImageBinder;
+import com.dqr.www.multitypestudy.weibo.content.SimpleImge;
 import com.dqr.www.multitypestudy.weibo.content.SimpleText;
 import com.dqr.www.multitypestudy.weibo.content.SimpleTextBinder;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import me.drakeet.multitype.Linker;
 import me.drakeet.multitype.MultiTypeAdapter;
 
 /**
@@ -36,15 +40,33 @@ public class WeiboActivity extends MenuBaseActivity {
 
 
         mAdapter = new MultiTypeAdapter();
-        mAdapter.register(Weibo.class, new SimpleTextBinder());
+        mAdapter.register(Weibo.class).to(
+                new SimpleTextBinder()
+                ,new SimpleImageBinder())
+                .withLinker(new Linker<Weibo>() {
+                    @Override
+                    public int index(@NonNull Weibo weibo) {
+                        if(weibo.mWeiboContent instanceof SimpleText){
+                            return 0;
+                        }else if(weibo.mWeiboContent instanceof SimpleImge){
+                            return 1;
+                        }
+                        return 0;
+                    }
+                });
+
 
         items = new ArrayList<>();
 
-        User user = new User("Bux",R.drawable.img_00);
-        for(int i=0;i<20;i++){
-            SimpleText simpleText = new SimpleText(1,"Just a simple text msg:"+i);
-            Weibo weibo = new Weibo(user,simpleText);
+        User user = new User("Bux", R.drawable.img_00);
+        for (int i = 0; i < 20; i++) {
+            SimpleText simpleText = new SimpleText(0, "Just a simple text msg:" + i);
+            Weibo weibo = new Weibo(user, simpleText);
             items.add(weibo);
+
+            SimpleImge simpleImage = new SimpleImge(1, R.drawable.img_01);
+            Weibo weibo1 = new Weibo(user, simpleImage);
+            items.add(weibo1);
         }
 
         mAdapter.setItems(items);
